e6332c32882efa6367df892dcd90a04736a4823b
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-12-27 08:47:05 -0800

    fix: modify TopProducts

--- libs/components/homepage/TopProperties.tsx
+++ libs/components/homepage/TopProperties.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { Stack, Box } from '@mui/material';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
-import WestIcon from '@mui/icons-material/West';
-import EastIcon from '@mui/icons-material/East';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation, Pagination } from 'swiper';
 import TopPropertyCard from './TopPropertyCard';
@@ -60,22 +59,29 @@ const TopProperties = (props: TopPropertiesProps) => {
 
 	if (device === 'mobile') {
 		return (
-			<Stack className={'top-properties'}>
+			<Stack className={'top-agents top-properties'}>
 				<Stack className={'container'}>
-					<Stack className={'info-box'}>
-						<span>Top properties</span>
+					<Stack className={'info-box top-agents__info'}>
+						<Box component={'div'} className={'top-agents__heading'}>
+							<span>Top Products</span>
+							<p>Check out our Top Products</p>
+						</Box>
 					</Stack>
-					<Stack className={'card-box'}>
+					<Stack className={'wrapper'}>
 						<Swiper
-							className={'top-property-swiper'}
+							className={'top-agents-swiper'}
 							slidesPerView={'auto'}
 							centeredSlides={true}
-							spaceBetween={15}
+							spaceBetween={18}
 							modules={[Autoplay]}
+							autoplay={{
+								delay: 3200,
+								disableOnInteraction: false,
+							}}
 						>
 							{topProducts.map((product: Product) => {
 								return (
-									<SwiperSlide className={'top-property-slide'} key={product?._id}>
+									<SwiperSlide className={'top-agents-slide'} key={product?._id}>
 										<TopPropertyCard product={product} likeProductHandler={likeProductHandler} />
 									</SwiperSlide>
 								);
@@ -87,43 +93,50 @@ const TopProperties = (props: TopPropertiesProps) => {
 		);
 	} else {
 		return (
-			<Stack className={'top-properties'}>
+			<Stack className={'top-agents top-properties'}>
 				<Stack className={'container'}>
-					<Stack className={'info-box'}>
-						<Box component={'div'} className={'left'}>
-							<span>Top properties</span>
-							<p>Check out our Top Properties</p>
-						</Box>
-						<Box component={'div'} className={'right'}>
-							<div className={'pagination-box'}>
-								<WestIcon className={'swiper-top-prev'} />
-								<div className={'swiper-top-pagination'}></div>
-								<EastIcon className={'swiper-top-next'} />
-							</div>
+					<Box component={'div'} className={'top-agents__halo top-agents__halo--left'} />
+					<Box component={'div'} className={'top-agents__halo top-agents__halo--right'} />
+					<Stack className={'info-box top-agents__info'}>
+						<Box component={'div'} className={'top-agents__head'}>
+							<Box component={'div'} className={'left'}>
+								<span>Top Products</span>
+								<p>Check out our Top Products</p>
+							</Box>
+							<Box component={'div'} className={'right'} />
 						</Box>
 					</Stack>
-					<Stack className={'card-box'}>
-						<Swiper
-							className={'top-property-swiper'}
-							slidesPerView={'auto'}
-							spaceBetween={15}
-							modules={[Autoplay, Navigation, Pagination]}
-							navigation={{
-								nextEl: '.swiper-top-next',
-								prevEl: '.swiper-top-prev',
-							}}
-							pagination={{
-								el: '.swiper-top-pagination',
-							}}
-						>
-							{topProducts.map((product: Product) => {
-								return (
-									<SwiperSlide className={'top-property-slide'} key={product?._id}>
-										<TopPropertyCard product={product} likeProductHandler={likeProductHandler} />
-									</SwiperSlide>
-								);
-							})}
-						</Swiper>
+					<Stack className={'wrapper'}>
+						<Box component={'div'} className={'switch-btn swiper-agents-prev'}>
+							<ArrowBackIosNewIcon />
+						</Box>
+						<Box component={'div'} className={'card-wrapper'}>
+							<Swiper
+								className={'top-agents-swiper'}
+								slidesPerView={'auto'}
+								spaceBetween={24}
+								modules={[Autoplay, Navigation, Pagination]}
+								navigation={{
+									nextEl: '.swiper-agents-next',
+									prevEl: '.swiper-agents-prev',
+								}}
+								autoplay={{
+									delay: 3600,
+									disableOnInteraction: false,
+								}}
+							>
+								{topProducts.map((product: Product) => {
+									return (
+										<SwiperSlide className={'top-agents-slide'} key={product?._id}>
+											<TopPropertyCard product={product} likeProductHandler={likeProductHandler} />
+										</SwiperSlide>
+									);
+								})}
+							</Swiper>
+						</Box>
+						<Box component={'div'} className={'switch-btn swiper-agents-next'}>
+							<ArrowBackIosNewIcon />
+						</Box>
 					</Stack>
 				</Stack>
 			</Stack>
